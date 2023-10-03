b0622a1c7af5afe36d49e51565fc9c35e0aca411
Prathamesh314 <124865398+Prathamesh314@users.noreply.github.com>
2023-10-03 20:45:46 +0530

    Initialized new Spring boot project

---
+++ src/main/java/com/kurveps/filemanagement/commands/FileCommands.java
@@ -0,0 +1,4 @@
+package com.kurveps.filemanagement.commands;
+
+public class FileCommands {
+}
---
+++ src/test/java/com/kurveps/filemanagement/CliForManagingFilesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kurveps.filemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CliForManagingFilesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
